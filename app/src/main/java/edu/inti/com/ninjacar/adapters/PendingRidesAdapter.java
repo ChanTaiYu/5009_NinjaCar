024c97b103cf4a2b9e706663e023b8a6d82c3184
opera <gith6503@gmail.com>
2024-11-26 21:12:57 +0800

    modify the code

--- app/src/main/java/edu/inti/com/ninjacar/adapters/PendingRidesAdapter.java
+++ app/src/main/java/edu/inti/com/ninjacar/adapters/PendingRidesAdapter.java
@@ -1,6 +1,77 @@
 package edu.inti.com.ninjacar.adapters;
 
-public class PendingRidesAdapter {
-    public void notifyItemRemoved(int rideId) {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import edu.inti.com.ninjacar.R;
+import edu.inti.com.ninjacar.datamodels.Ride;
+
+public class PendingRidesAdapter extends RecyclerView.Adapter<PendingRidesAdapter.PendingRidesViewHolder> {
+
+    private final List<Ride> pendingRides;
+    private final OnRideActionListener listener;
+
+    public interface OnRideActionListener {
+        void onDeleteRide(Ride ride);
+        void onViewRideDetails(Ride ride);
+    }
+
+    public PendingRidesAdapter(List<Ride> pendingRides, OnRideActionListener listener) {
+        this.pendingRides = pendingRides;
+        this.listener = listener;
+    }
+
+    @NonNull
+    @Override
+    public PendingRidesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_pending_rides, parent, false);
+        return new PendingRidesViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PendingRidesViewHolder holder, int position) {
+        Ride ride = pendingRides.get(position);
+        holder.bind(ride, listener);
+    }
+
+    @Override
+    public int getItemCount() {
+        return pendingRides.size();
+    }
+
+    public static class PendingRidesViewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView tvRideDate;
+        private final TextView tvRideTime;
+        private final TextView tvRideDestination;
+        private final ImageButton btnDeleteRide;
+        private final ImageButton btnViewDetails;
+
+        public PendingRidesViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvRideDate = itemView.findViewById(R.id.tv_ride_date);
+            tvRideTime = itemView.findViewById(R.id.tv_ride_time);
+            tvRideDestination = itemView.findViewById(R.id.tv_ride_destination);
+            btnDeleteRide = itemView.findViewById(R.id.btn_delete_ride);
+            btnViewDetails = itemView.findViewById(R.id.btn_view_details);
+        }
+
+        public void bind(Ride ride, OnRideActionListener listener) {
+            tvRideDate.setText(ride.getDate());
+            tvRideTime.setText(ride.getTime());
+            tvRideDestination.setText(ride.getDestination());
+
+            btnDeleteRide.setOnClickListener(v -> listener.onDeleteRide(ride));
+            btnViewDetails.setOnClickListener(v -> listener.onViewRideDetails(ride));
+        }
     }
 }
