44af135672bc8c581790c82eb3fdb9f591b91e4e
Rachel Ng Ker Xin <gith6503@gmail.com>
2024-11-26 22:25:39 +0800

    Add files via upload

---
+++ app/src/main/java/edu/inti/com/ninjacar/firebaseutils/DeleteRideCallBack.java
@@ -0,0 +1,23 @@
+package edu.inti.com.ninjacar.firebaseutils;
+
+import edu.inti.com.ninjacar.datamodels.Ride;
+
+/**
+ * Callback interface for handling the deletion of a ride from Firebase or any data source.
+ */
+public interface DeleteRideCallBack<Ride> {
+
+    /**
+     * Called when a ride has been successfully deleted.
+     *
+     * @param ride The ride that was deleted.
+     */
+    void onRideDeleted(Ride ride);
+
+    /**
+     * Called when there is an error during the deletion of a ride.
+     *
+     * @param errorMessage The error message explaining the failure.
+     */
+    void onDeleteError(String errorMessage);
+}
